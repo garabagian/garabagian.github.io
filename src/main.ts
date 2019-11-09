b0fcce3357a18f494b5f6af497d984d5455b4451
garabagian <garabagian@gmail.com>
2019-11-08 23:09:50 -0800

    Putting angular app at root directory to be able to build it.

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import { Tree } from './structures/tree'
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+  function main() {
+    var treeCanvas = <HTMLCanvasElement>document.getElementById('tree');
+    treeCanvas.width = document.documentElement.clientWidth;
+    treeCanvas.height = document.documentElement.clientHeight;
+    var check = new Tree(treeCanvas);
+    window.addEventListener("resize", (e: Event) => {
+      treeCanvas.width = document.documentElement.clientWidth;
+      treeCanvas.height = document.documentElement.clientHeight;
+      console.log(treeCanvas.width + " , " + treeCanvas.height);
+      if(check.ball.y+check.ball.radius>=treeCanvas.height){
+        check.ball.y=treeCanvas.height-check.ball.radius;
+      }
+    }, false);
+
+
+
+  }
+  main();
